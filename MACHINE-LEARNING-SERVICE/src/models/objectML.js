053b7aa4401995af829993342b16f2a8787a4702
Steven Ospina <stohk93@gmail.com>
2022-01-27 16:55:25 -0500

    code review: fixed

--- MACHINE-LEARNING-SERVICE/src/models/objectML.js
+++ MACHINE-LEARNING-SERVICE/src/models/objectML.js
@@ -2,10 +2,10 @@ const cocoSsd = require('@tensorflow-models/coco-ssd');
 const tf = require('@tensorflow/tfjs-node');
 const fs = require('fs').promises;
 class ObjectML{
-  constructor (image,percentage,objectRequired){
+  constructor (image, percentage, objectRequired){
     this.image = image;
     this.percentage = percentage;
-    this.objectRequired =objectRequired;
+    this.objectRequired = objectRequired;
   }
 
   async predict(){
@@ -14,9 +14,9 @@ class ObjectML{
     const imgTensor = tf.node.decodeImage(new Uint8Array(results[1]), 3);
     const predictions = await model.detect(imgTensor);
     const arrayObjects = [];
-    predictions.every((prediction)=>{
+    predictions.every((prediction)=> {
       if(this.objectRequired == prediction.class && prediction.score >= this.percentage){
-        arrayObjects.push({"object": prediction.class, "Score": prediction.score});
+        arrayObjects.push({'object': prediction.class, 'Score': prediction.score});
         return false;
       }else{
         return true;
