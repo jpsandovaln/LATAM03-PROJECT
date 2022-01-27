dbbb158c545596a02ae36be88050879321ffca3d
Steven Ospina <stohk93@gmail.com>
2022-01-27 16:00:16 -0500

    First commit, class object recognized

---
+++ MACHINE-LEARNING-SERVICE/src/models/objectML.js
@@ -0,0 +1,28 @@
+const cocoSsd = require('@tensorflow-models/coco-ssd');
+const tf = require('@tensorflow/tfjs-node');
+const fs = require('fs').promises;
+class objectML{
+  constructor (image,percentage,objectRequired){
+    this.image = image;
+    this.percentage = percentage;
+    this.objectRequired =objectRequired;
+  }
+
+  async predict(){
+    const results = await Promise.all([cocoSsd.load(), fs.readFile(this.image)]);
+    const model = results[0];
+    const imgTensor = tf.node.decodeImage(new Uint8Array(results[1]), 3);
+    const predictions = await model.detect(imgTensor);
+    const arrayObjects = [];
+    predictions.every((prediction)=>{
+      if(this.objectRequired == prediction.class && prediction.score >= this.percentage){
+        arrayObjects.push({"object": prediction.class, "Score": prediction.score});
+        return false;
+      }else{
+        return true;
+      }
+    });
+    var arrayObjects = [{"object": predictions[0].class, "Score": predictions[0].score}];
+    return arrayObjects;
+    }
+}
