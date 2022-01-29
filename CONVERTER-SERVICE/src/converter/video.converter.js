25b50f1926e36999581a39f1bb84560f833f2fcb
florpadilla <florpadilla.93@gmail.com>
2022-01-29 20:46:51 -0300

    add README and changed the VideoConverter Class

---
+++ CONVERTER-SERVICE/src/converter/video.converter.js
@@ -0,0 +1,38 @@
+/*
+@ObjectML.js Copyright (c) 2022 Jalasoft
+CI 26 Sur #48-41, Ayurá Center, Edificio Unión № 1376, Medellín, Colombia
+All rights reserved
+This software is the confidential and proprietary information of
+Jalasoft Confidential Information You shall not
+disclose such Confidential Information and shall use it only in
+accordance with the terms of the license agreement you entered into
+with Jalasoft.
+*/
+
+const FfmpegCommand = require('fluent-ffmpeg');
+
+//This class represents a model for convert Video into Images 
+class VideoConverter {
+
+  //This method allows to convert a video into an specif number of images by setting the Frames Per Second with an specific size 
+  static converter({input, fps, finalSize}) {
+    return new Promise(function(resolve, reject){
+      FfmpegCommand(`../files/videos/${input}`)
+      .size(finalSize)
+      .fps(fps)
+      .save('../files/images/%3d.jpg')
+      .on('start',function(){
+        console.log('Processing starting...');
+      })
+      .on('error', function(err){
+        reject(err);
+      })
+      .on('end', function(){       
+        console.log('Processing finished! ');
+        resolve(true);
+      })
+    })
+  }
+}
+
+module.exports = VideoConverter;
