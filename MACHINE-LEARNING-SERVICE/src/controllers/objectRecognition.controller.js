170fb1b7a2abed4922e7ffb8845b02ef8fa77e43
DilanOjeda <dilanangel12@gmail.com>
2022-01-29 15:24:54 -0400

    docs: Correct comments of the clases

--- MACHINE-LEARNING-SERVICE/src/controllers/objectRecognition.controller.js
+++ MACHINE-LEARNING-SERVICE/src/controllers/objectRecognition.controller.js
@@ -1,27 +1,43 @@
+/*
+@objectRecognition.controller.js Copyright (c) 2022 Jalasoft
+2643 Av Melchor Perez de Olguin Colquiri Sud, Cochabamba, Bolivia.
+Av. General Inofuentes esquina Calle 20,Edificio Union № 1376, La Paz, Bolivia
+All rights reserved
+This software is the confidential and proprietary information of
+Jalasoft Confidential Information You shall not
+disclose such Confidential Information and shall use it only in
+accordance with the terms of the license agreement you entered into
+with Jalasoft
+*/
+
 const path = require('path');
 const ObjectML = require('../models/objectML');
 
-class ObjectRecognitionController{
-
-    static recognizeObjects = async (req, res) => {
-        const {files: images} = req;
-        const modelOne = new ObjectML(path.join(__dirname, '../uploads/images/Dogs_cats.jpg'), 0.80, 'dog');
-        const result = await modelOne.predict();
-        if (result.length === 0) {
-            res.json({msg: 'There is not the object in the image.'});
-        }
-        return res.json(result);
-    }
+// Represents a controller layer 
+class ObjectRecognitionController {
     
-    static downloadImage = (req, res) => {
-        const {image} = req.params;
-        const path = './uploads/images/' + image;
-        res.download(path, (err) => {
-            if (err) {
-                return res.json({msg: err});
-            }
-        });
+  // Recognizes an object calling ObjectML class
+  static async recognizeObjects(req, res) {
+    const { files: images } = req;
+    const modelOne = new ObjectML(
+      path.join(__dirname, '../uploads/images/Dogs_cats.jpg'), 0.8, 'dog');
+    const result = await modelOne.predict();
+    if (result.length === 0) {
+      return res.json({msg: 'There is not the object in the image.'});
     }
+    res.json({result});
+  }
+
+  // Downloads an image from uploads file
+  static downloadImage(req, res) {
+    const { image } = req.params;
+    const path = './uploads/images/' + image;
+    res.download(path, (err) => {
+      if (err) {
+        return res.json({ msg: err });
+      }
+    });
+  }
 }
 
 module.exports = ObjectRecognitionController;
--- MACHINE-LEARNING-SERVICE/src/controllers/server.controller.js
+++ MACHINE-LEARNING-SERVICE/src/controllers/server.controller.js
@@ -1,28 +1,46 @@
+/*
+@server.controller.js Copyright (c) 2022 Jalasoft
+2643 Av Melchor Perez de Olguin Colquiri Sud, Cochabamba, Bolivia.
+Av. General Inofuentes esquina Calle 20,Edificio Union № 1376, La Paz, Bolivia
+All rights reserved
+This software is the confidential and proprietary information of
+Jalasoft Confidential Information You shall not
+disclose such Confidential Information and shall use it only in
+accordance with the terms of the license agreement you entered into
+with Jalasoft
+*/
+
 const express = require('express');
 const cors = require('cors');
 
-class Server{
-    constructor(port, path){
-        this._app = express();
-        this._port = port
-        this._path = path;
+// Represents a server to be set up
+class Server {
+  
+  constructor(port, path) {
+    this._app = express();
+    this._port = port;
+    this._path = path;
+    this.middleware();
+    this.routes();
+  }
 
-        this.middleware();
-        this.routes();
-    }
-    
-    middleware(){
-        this._app.use(cors());
-        this._app.use(express.json());
-    }
+  // Executes middleware of the server 
+  middleware() {
+    this._app.use(cors());
+    this._app.use(express.json());
+  }
 
-    routes(){
-        this._app.use(this._path, require('../routes/objectRecognition.routes'));
-    }
+  // Redirects to the project routes
+  routes() {
+    this._app.use(this._path, require('../routes/objectRecognition.routes'));
+  }
 
-    listen(){
-        this._app.listen(this._port, () => console.info(`It running on port ${this._port}`));
-    }
+  // Listens the connection to create a server
+  listen() {
+    this._app.listen(this._port, () =>
+      console.info(`It running on port ${this._port}`)
+    );
+  }
 }
 
 module.exports = Server;
