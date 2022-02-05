54d5cc547d99337e916264801584dad305d582cd
DilanOjeda <dilanangel12@gmail.com>
2022-02-05 17:23:33 -0400

    Docs: Add Copyright comment in ImageConverter class

--- CONVERTER-SERVICE/src/models/imageConverter.model.js
+++ CONVERTER-SERVICE/src/models/imageConverter.model.js
@@ -1,3 +1,14 @@
+/*
+@Video.converter.js Copyright (c) 2022 Jalasoft
+CI 26 Sur #48-41, Ayurá Center, Edificio Unión № 1376, Medellín, Colombia
+All rights reserved
+This software is the confidential and proprietary information of
+Jalasoft Confidential Information You shall not
+disclose such Confidential Information and shall use it only in
+accordance with the terms of the license agreement you entered into
+with Jalasoft.
+*/
+
 const sharp =  require('sharp');
 const Converter = require('./converter');
 
