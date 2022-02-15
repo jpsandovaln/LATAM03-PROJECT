71c07375fd44a3535409d61a6d2ff6d14d6e8b78
DilanOjeda <dilanangel12@gmail.com>
2022-02-15 17:36:38 -0400

    Docs: remove comments

--- CONVERTER-SERVICE/src/controllers/compositer.controller.js
+++ CONVERTER-SERVICE/src/controllers/compositer.controller.js
@@ -22,7 +22,6 @@ class CompositerController {
   
   // Executes and verifies the response of the 3 methods involved in the project
   static async compositeImages(req, res) {
-    console.log('req.files', req.files)
     const top = Number(req.body.top);
     const left = Number(req.body.left);
     const format = req.body.format;
