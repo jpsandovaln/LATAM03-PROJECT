4b771b97a99fadad1428ba9bfca430830d65bb33
Steven Ospina <stohk93@gmail.com>
2022-02-17 17:00:18 -0500

    change name cocossd.mode.test

--- MACHINE-LEARNING-SERVICE/src/model/models/cocoSsd.model.test.js
+++ MACHINE-LEARNING-SERVICE/src/model/models/cocoSsd.model.test.js
@@ -1,5 +1,5 @@
 /*
-@model.facade.js Copyright (c) 2022 Jalasoft
+@cocoSsd.model.test.js Copyright (c) 2022 Jalasoft
 CI 26 Sur #48-41, Ayurá Center, Edificio Unión № 1376, Medellín, Colombia
 All rights reserved
 This software is the confidential and proprietary information of
