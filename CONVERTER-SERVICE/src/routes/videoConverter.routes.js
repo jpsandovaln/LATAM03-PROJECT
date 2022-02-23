7278ca86db118fac8c39e80365a4d33af977bf4a
DilanOjeda <dilanangel12@gmail.com>
2022-02-23 15:57:26 -0400

    Add documents

--- CONVERTER-SERVICE/src/routes/videoConverter.routes.js
+++ CONVERTER-SERVICE/src/routes/videoConverter.routes.js
@@ -16,9 +16,12 @@ const VideoConverterController = require('../controllers/videoConverter.controll
 const FileValidator = require('../middlewares/fileValidator.middleware');
 const UploadProcessor = require('../middlewares/uploadProcessor.middleware');
 
-router.post('/', [
-  UploadProcessor.processUploadFile(),
-  FileValidator.validateUploadOneFile
-], VideoConverterController.convert);
+const upload = require('../middlewares/multer.middleware')
+router.post('/', upload(), VideoConverterController.convert);
+
+// router.post('/', [
+//   UploadProcessor.processUploadFile(),
+//   FileValidator.validateUploadOneFile
+// ], VideoConverterController.convert);
 
 module.exports = router;
